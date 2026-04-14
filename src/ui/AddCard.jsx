2ce8ca84bf11891a1ba613f85bf748d4e2eb5e64
hazrat <tora59459@gmail.com>
2026-04-14 18:32:50 +0600

    finally cheaking and my coded

--- src/ui/AddCard.jsx
+++ src/ui/AddCard.jsx
@@ -19,14 +19,13 @@ const AddCard = ({ data }) => {
         <div>
             <div className='flex gap-4 bg-[#E9E9E9] rounded-lg py-4 px-5 mb-6'>
                 <img src={statusIcon} alt={data.type} className="w-10 h-10" />
-                
+
                 <div>
+
                     <p>
-                        <span className='text-[#244D3F] text-lg font-medium'>
-                           March {currentDate}
-                        </span> with {data.name}
+                        {`${data.type === 'call' ? 'Call' : data.type === 'text' ? 'Text' : 'Video'} with ${data.name}`}
                     </p>
-                    <p className='text-[#64748B]'>{currentDate}</p>
+                    <p className='text-[#64748B]'>March {currentDate}</p>
                 </div>
             </div>
         </div>
